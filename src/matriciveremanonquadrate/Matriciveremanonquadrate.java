af6851dc4bbec0b47af5465a4fe503e4da3d1840
Lorenzo <lorenzotardini@gmail.com>
2020-02-05 19:06:47 +0100

    huh, nice

---
+++ src/matriciveremanonquadrate/Matriciveremanonquadrate.java
@@ -0,0 +1,62 @@
+package matriciveremanonquadrate;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Matriciveremanonquadrate {
+
+    public static void main(String[] args) {
+        int colonne = 0, righe = 0;
+        Scanner scenario = new Scanner(System.in);
+        Random rando = new Random();
+
+        System.out.print("righe? ");
+        righe = scenario.nextInt();
+        System.out.print("colonne? ");
+        colonne = scenario.nextInt();
+        int matro[][] = new int[righe][colonne];
+
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                matro[i][j] = rando.nextInt(10);
+            }
+        }
+
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (i % 2 == 0) {
+                    System.out.print(matro[i][j] + " ");
+                }
+                if (i % 2 == 1) {
+                    if (j % 2 == 0) {
+                        System.out.print(matro[i][j] + " ");
+                    }
+                    if (j % 2 == 1) {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("  ");
+                }
+                if (i % 2 == 1) {
+                    if (j % 2 == 0) {
+                        System.out.print("  ");
+                    }
+                    if (j % 2 == 1) {
+                        System.out.print(matro[i][j]);
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
